e92854a45631cb292e423f8d83eb5c04fe9b2d73
Jay Logan <jay@sixy.name>
2019-01-25 19:19:04 -0600

    Remove sudo check from bastion.sh. Allows running as own user.

--- bastion.sh
+++ bastion.sh
@@ -7,16 +7,6 @@ echo
 source ./scripts/bash/definitions.sh
 source ./scripts/bash/logger.sh
 
-if [ -n "$SUDO_USER" ]
-then
-  echo
-  print::error "Root permission detected"
-  print::message "I do not need root permissions. Please run without using sudo."
-  echo
-
-  exit 1
-fi
-
 source ./scripts/bash/welcome.sh
 source ./scripts/bash/checks.sh
 source ./scripts/bash/methods.sh
