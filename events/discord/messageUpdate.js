0b35b93d34e35c9e7a05c1b18418f0fe0ffca1f2
Jay Logan <jay@sixy.name>
2019-01-27 00:06:57 -0600

    Kill logging of message edits and deletes The logging for these events significantly lowers signal-to-noise ratio and intentionally doesn't log the contents per TheBastionBot/Bastion#538. Tracking of edits is more suited to logging bots dumping to text files, such as panopticon.

--- events/discord/messageUpdate.js
+++ events/discord/messageUpdate.js
@@ -28,61 +28,6 @@ module.exports = async  (oldMessage, newMessage) => {
 
     // Filter Discord server invites from the message
     inviteFilter(newMessage);
-
-    // Server Logging
-    let guildModel = await newMessage.client.database.models.guild.findOne({
-      attributes: [ 'serverLog' ],
-      where: {
-        guildID: newMessage.guild.id
-      }
-    });
-
-    if (!guildModel || !guildModel.dataValues.serverLog) return;
-
-    let logChannel = newMessage.guild.channels.get(guildModel.dataValues.serverLog);
-    if (!logChannel) return;
-
-    logChannel.send({
-      embed: {
-        color: newMessage.client.colors.ORANGE,
-        title: newMessage.client.i18n.event(newMessage.guild.language, 'messageUpdate'),
-        fields: [
-          {
-            name: 'Message Channel',
-            value: newMessage.channel.toString(),
-            inline: true
-          },
-          {
-            name: 'Message Channel ID',
-            value: newMessage.channel.id,
-            inline: true
-          },
-          {
-            name: 'Message ID',
-            value: newMessage.id,
-            inline: true
-          },
-          {
-            name: 'Message Author',
-            value: newMessage.author.tag,
-            inline: true
-          },
-          {
-            name: 'Message Author ID',
-            value: newMessage.author.id,
-            inline: true
-          },
-          {
-            name: 'Old Message Content',
-            value: '~~Deleted~~',
-            inline: true
-          }
-        ],
-        timestamp: new Date()
-      }
-    }).catch(e => {
-      newMessage.client.log.error(e);
-    });
   }
   catch (e) {
     newMessage.client.log.error(e);
